41616dbc3078394d1eab00062f66c505805711b7
MichaelWehar <mwehar@buffalo.edu>
2017-09-12 05:01:29 -0400

    Added Homework 1 Starter Code

---
+++ hw1/Selector.java
@@ -0,0 +1,21 @@
+
+public class Selector {
+	
+	private static void swap(Word[] array, int i, int j){
+		if(i == j) return;
+		
+		Word temp = array[i];
+		array[i] = array[j];
+		array[j] = temp;
+	}
+	
+	public static Word[] select(Word[] array, int k){
+	// Problem #1
+	// Fill in this method with an O(n*k) time algorithm
+	// that returns the k largest elements of array in
+	// order from largest to smallest.
+	// Note: This should return an array with k elements.
+	
+		return null;
+	}
+}
