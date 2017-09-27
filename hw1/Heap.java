f970787ed564691dc71104564b72999b693ab7f3
Harlie Curcio <hmc26@pitt.edu>
2017-09-26 23:05:55 -0400

    Heap

--- hw1/Heap.java
+++ hw1/Heap.java
@@ -1,6 +1,10 @@
+import java.lang.*;
+import java.util.Arrays;
 
 public class Heap {
 	
+	private int[] finalArray;
+
 	public Heap(Word[] array){
 		buildHeap(array);
 	}
@@ -11,6 +15,71 @@ public class Heap {
 	// that builds an n element complete binary heap.
 	// Note: You are allowed to add and modify fields
     // and helper methods.
+		String[] stringArr = new String[array.length];
+		String[] strings = new String[array.length];
+		finalArray = new int[array.length];
+		for(int i=0; i<array.length; i++)
+		{
+			stringArr[i] = "" + array[i];
+			strings[i] = stringArr[i];
+			stringArr[i] = stringArr[i].substring(stringArr[i].indexOf(" ")+1);
+			stringArr[i] = stringArr[i].substring(0, stringArr[i].indexOf("]"));
+			finalArray[i] = Integer.parseInt(stringArr[i]);
+		}
+
+		int n = array.length;
+
+		for(int i=n/2-1; i>=0; i--)
+		{
+			heapify(finalArray, n, i, array);
+		}
+
+		for(int i=n-1; i>=0; i--)
+		{
+			int temp = finalArray[0];
+			finalArray[0] = finalArray[i];
+			finalArray[i] = temp;
+			heapify(finalArray, i, 0, array);
+		}
+
+		String[] numbers = new String[array.length];
+
+		for(int i=0; i<array.length; i++)
+		{
+			numbers[i] = Integer.toString(finalArray[i]);
+		}
+	}
+
+	static void printArray(Word array[])
+	{
+		int n= array.length;
+		for(int i=0; i<n; ++i)
+		{
+			System.out.print(array[i]+" ");
+		}
+		System.out.println();
+	}
+
+	public void heapify(int[] finalArray, int n, int i, Word[] array)
+	{
+		int min = i;
+		int leftChild = 2*i+1;
+		int rightChild = 2*i+2;
+
+		if(leftChild<n && finalArray[leftChild] < finalArray[min])
+		{
+			min=leftChild;
+		}
+		if(rightChild<n && finalArray[rightChild] < finalArray[min])
+		{
+			min=rightChild;
+		}
+		if(min!=i)
+		{
+			int temp = finalArray[i];
+			finalArray[i] = finalArray[min];
+			heapify(finalArray, n, min, array);
+		}
 	}
 	
 	public Word removeMax(){
@@ -18,7 +87,10 @@ public class Heap {
 	// Fill in this method with an O(log(n)) time algorithm
 	// that removes the root element, restores the heap
 	// structure, and finally returns the removed root element.
-		
+		/*Word popped = array[front];
+		array[front] = array[count--];
+
+		return popped;*/
 		return null;
 	}
 	
@@ -29,4 +101,22 @@ public class Heap {
 		}
 		return result;
 	}
+
+	/*public int parent(int i){
+		int par;
+		par = (i-1)/2;
+		return par;
+	}
+
+	public int leftChild(int i){
+		int lChild;
+		lChild = (2*i)+1;
+		return lChild;
+	}
+
+	public int rightChild(int i){
+		int rChild;
+		rChild = (2*i)+2;
+		return rChild;
+	}*/
 }
--- hw1/Selector.java
+++ hw1/Selector.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 public class Selector {
 	
@@ -15,7 +16,40 @@ public class Selector {
 	// that returns the k largest elements of array in
 	// order from largest to smallest.
 	// Note: This should return an array with k elements.
-	
-		return null;
+	//Word[] temp = WordReader.loadWords();
+	String[] stringWord = new String[array.length];
+	int[] wordFreq = new int[array.length];
+	int counter=0;
+	for(int i=0; i<array.length; i++)
+	{
+		stringWord[i] = ""+array[i];
+		stringWord[i] = stringWord[i].substring(stringWord[i].indexOf(" ")+1);
+		stringWord[i] = stringWord[i].substring(0,stringWord[i].indexOf("]"));
+		wordFreq[i] = Integer.parseInt(stringWord[i]);
+	}
+
+	for(int i=0; i<array.length-1; i++)
+	{
+		counter++;
+		int min = i;
+		for(int j= i+1; j<array.length; j++)
+		{
+			if(wordFreq[j]>wordFreq[min])
+			{
+				min=j;
+			}
+		}
+
+		int tester = wordFreq[min];
+		wordFreq[min] = wordFreq[i];
+		wordFreq[i] = tester;
+
+		if(counter <= 500)
+		{
+			swap(array, i, min);
+		}
+	}
+
+	return Arrays.copyOfRange(array, 0, 499);
 	}
 }
