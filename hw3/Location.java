314ae34e2f8e563954f39eea8fdd049d72670271
MichaelWehar <mwehar@buffalo.edu>
2017-11-01 10:58:44 -0400

    HW3 Starter Code

---
+++ hw3/Location.java
@@ -0,0 +1,16 @@
+
+public class Location {
+
+	public String name;
+	
+	public Location(String name){
+		this.name = name;
+	}
+	
+	@Override
+	public boolean equals(Object obj){
+		if(obj == null || !(obj instanceof Location)) return false;
+		else return this.name.equals(((Location)obj).name);
+	}
+	
+}
